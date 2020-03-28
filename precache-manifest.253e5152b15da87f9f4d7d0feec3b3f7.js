a83248e584e96f890babe0c3da6c753d03c733d6
Michael <michael.digiorgio70@qmail.cuny.edu>
2020-03-27 22:59:20 -0400

    Updates

---
+++ precache-manifest.253e5152b15da87f9f4d7d0feec3b3f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c926e9b1f14996fef0e95ef1ad5f484",
+    "url": "/covidnyc/index.html"
+  },
+  {
+    "revision": "99bcc6ba7efe7ec8d116",
+    "url": "/covidnyc/static/css/main.8188d4da.chunk.css"
+  },
+  {
+    "revision": "fe6ca4f5fcea0f82f747",
+    "url": "/covidnyc/static/js/2.1058f48c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covidnyc/static/js/2.1058f48c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99bcc6ba7efe7ec8d116",
+    "url": "/covidnyc/static/js/main.a0884027.chunk.js"
+  },
+  {
+    "revision": "170b7a2d53865da0f7d6",
+    "url": "/covidnyc/static/js/runtime-main.f99af8b6.js"
+  }
+]);
